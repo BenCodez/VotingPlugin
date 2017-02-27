71bda92e466af278b96f5dfb6d12d9a934a4bff1
Ben12345rocks <benbergen12@gmail.com>
2017-02-27 16:21:40 -0500

    Fix signs not properly being removed, closes #168

--- VotingPlugin/src/com/Ben12345rocks/VotingPlugin/Events/BlockBreak.java
+++ VotingPlugin/src/com/Ben12345rocks/VotingPlugin/Events/BlockBreak.java
@@ -1,7 +1,7 @@
 package com.Ben12345rocks.VotingPlugin.Events;
 
-import java.util.Set;
-
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
 import org.bukkit.block.Sign;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
@@ -9,7 +9,7 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
 
 import com.Ben12345rocks.VotingPlugin.Main;
-import com.Ben12345rocks.VotingPlugin.Data.ServerData;
+import com.Ben12345rocks.VotingPlugin.Objects.SignHandler;
 
 // TODO: Auto-generated Javadoc
 /**
@@ -18,7 +18,6 @@ import com.Ben12345rocks.VotingPlugin.Data.ServerData;
 public class BlockBreak implements Listener {
 
 	/** The plugin. */
-	@SuppressWarnings("unused")
 	private static Main plugin;
 
 	/**
@@ -41,16 +40,19 @@ public class BlockBreak implements Listener {
 	public void onBlockBreak(BlockBreakEvent event) {
 		if (event.getBlock().getState() instanceof Sign) {
 			Sign s = (Sign) event.getBlock().getState();
-			if (s.getLine(0).startsWith("TopVoter")) {
-				Set<String> signs = ServerData.getInstance().getSigns();
-				if (signs != null) {
-					for (String sign : signs) {
-						if (ServerData.getInstance().getSignLocation(sign).equals(event.getBlock().getLocation())) {
-							ServerData.getInstance().removeSign(sign);
+			final Location loc = s.getLocation();
+			Bukkit.getScheduler().runTaskAsynchronously(plugin, new Runnable() {
+
+				@Override
+				public void run() {
+					for (SignHandler sign : plugin.signs) {
+						if (sign.getLocation().equals(loc)) {
+							sign.removeSign();
+							sign.setValid(false);
 						}
 					}
 				}
-			}
+			});
 		}
 
 	}
