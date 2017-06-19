3a95ef9b2314523c4c44bd245d846e231d4337cd
Ben12345rocks <benbergen12@gmail.com>
2017-06-18 20:16:44 -0400

    Slight improvement

--- VotingPlugin/src/com/Ben12345rocks/VotingPlugin/Main.java
+++ VotingPlugin/src/com/Ben12345rocks/VotingPlugin/Main.java
@@ -805,31 +805,33 @@ public class Main extends JavaPlugin {
 	/**
 	 * Update.
 	 */
-	public synchronized void update() {
+	public void update() {
 		if (update && plugin != null) {
 			Bukkit.getScheduler().runTaskAsynchronously(plugin, new Runnable() {
 
 				@Override
 				public void run() {
-					if (AdvancedCoreHook.getInstance().getStorageType().equals(UserStorage.MYSQL)) {
-						if (AdvancedCoreHook.getInstance().getMysql() == null) {
-							plugin.debug("MySQL not loaded yet");
-							return;
-						} else {
-							AdvancedCoreHook.getInstance().getMysql().clearCache();
+					synchronized (plugin) {
+						if (AdvancedCoreHook.getInstance().getStorageType().equals(UserStorage.MYSQL)) {
+							if (AdvancedCoreHook.getInstance().getMysql() == null) {
+								plugin.debug("MySQL not loaded yet");
+								return;
+							} else {
+								AdvancedCoreHook.getInstance().getMysql().clearCache();
+							}
 						}
-					}
-					update = false;
-					plugin.debug("Starting background task");
-					try {
-						TopVoterHandler.getInstance().updateTopVoters();
-						Commands.getInstance().updateVoteToday();
-						ServerData.getInstance().updateValues();
-						Signs.getInstance().updateSigns();
-						plugin.debug("Background task ran");
-					} catch (Exception ex) {
-						ex.printStackTrace();
-						plugin.getLogger().info("Looks like something went wrong.");
+						update = false;
+						plugin.debug("Starting background task");
+						try {
+							TopVoterHandler.getInstance().updateTopVoters();
+							Commands.getInstance().updateVoteToday();
+							ServerData.getInstance().updateValues();
+							Signs.getInstance().updateSigns();
+							plugin.debug("Background task ran");
+						} catch (Exception ex) {
+							ex.printStackTrace();
+							plugin.getLogger().info("Looks like something went wrong.");
+						}
 					}
 				}
 
