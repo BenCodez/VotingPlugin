a346f28df136465ea727d79a5384a0937c36d253
Ben12345rocks <benbergen12@gmail.com>
2018-07-18 17:00:56 -0400

    Bug fix

--- VotingPlugin/src/com/Ben12345rocks/VotingPlugin/Objects/VoteSite.java
+++ VotingPlugin/src/com/Ben12345rocks/VotingPlugin/Objects/VoteSite.java
@@ -91,7 +91,8 @@ public class VoteSite {
 			String bc = StringUtils.getInstance().colorize(config.getFormatBroadCastMsg());
 			HashMap<String, String> placeholders = new HashMap<String, String>();
 			placeholders.put("player", playerName);
-			placeholders.put("nickname", user.getPlayer().getDisplayName());
+			placeholders.put("nickname",
+					(user.getPlayer() != null) ? user.getPlayer().getDisplayName() : user.getPlayerName());
 			placeholders.put("sitename", getDisplayName());
 			bc = StringUtils.getInstance().replacePlaceHolder(bc, placeholders);
 			ArrayList<Player> players = new ArrayList<Player>();
