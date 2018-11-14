9dd8e757d410b721be35b0dfed458dd78e92557b
Ben12345rocks <benbergen12@gmail.com>
2018-11-13 19:26:00 -0500

    Fix typo

--- VotingPlugin/src/com/Ben12345rocks/VotingPlugin/Commands/CommandLoader.java
+++ VotingPlugin/src/com/Ben12345rocks/VotingPlugin/Commands/CommandLoader.java
@@ -972,7 +972,7 @@ public class CommandLoader {
 
 						plugin.getCommand("adminvote" + arg)
 								.setTabCompleter(new AliasesTabCompleter().setCMDHandle(cmdHandle, true));
-						plugin.getCommand("vote" + arg).setPermission(cmdHandle.getPerm().split("|")[0]);
+						plugin.getCommand("adminvote" + arg).setPermission(cmdHandle.getPerm().split("|")[0]);
 					} catch (Exception ex) {
 						plugin.debug("Failed to load command and tab completer for /adminvote" + arg + ": "
 								+ ex.getMessage());
