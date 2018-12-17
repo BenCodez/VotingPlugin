eec877623edc948c40c03cfe4b1d0ea3ccba38fb
Ben12345rocks <benbergen12@gmail.com>
2018-12-16 19:15:48 -0500

    Add a class to make voting plugin hooks a lot easier

---
+++ VotingPlugin/src/com/Ben12345rocks/VotingPlugin/VotingPluginHooks.java
@@ -0,0 +1,26 @@
+package com.Ben12345rocks.VotingPlugin;
+
+import com.Ben12345rocks.AdvancedCore.Rewards.RewardHandler;
+import com.Ben12345rocks.AdvancedCore.Rewards.Injected.RewardInject;
+import com.Ben12345rocks.VotingPlugin.UserManager.UserManager;
+
+public class VotingPluginHooks {
+	private static VotingPluginHooks instance = new VotingPluginHooks();
+
+	public VotingPluginHooks getInstance() {
+		return instance;
+	}
+
+	public Main getMainClass() {
+		return Main.plugin;
+	}
+
+	public void addCustomReward(RewardInject inject) {
+		RewardHandler.getInstance().getInjectedRewards().add(inject);
+	}
+
+	public UserManager getUserManager() {
+		return UserManager.getInstance();
+	}
+
+}
