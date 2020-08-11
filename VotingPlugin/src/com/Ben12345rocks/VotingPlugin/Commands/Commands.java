ce3a40a15588a01b38a949009224dd3a4a134ace
Ben12345rocks <benbergen12@gmail.com>
2020-08-11 19:06:16 -0400

    Little change

--- VotingPlugin/src/com/Ben12345rocks/VotingPlugin/Commands/Commands.java
+++ VotingPlugin/src/com/Ben12345rocks/VotingPlugin/Commands/Commands.java
@@ -769,7 +769,7 @@ public class Commands {
 				}
 			}
 		} else {
-			now = now.plusHours(-(long) voteSite.getTimeOffSet());
+			lastVote = lastVote.plusHours((long) voteSite.getTimeOffSet());
 			if (lastVote.getDayOfYear() == now.getDayOfYear() && lastVote.getYear() == now.getYear()) {
 				LocalDateTime midnight = TimeChecker.getInstance().getTime().plusDays(1).withHour(0).withMinute(0)
 						.plusHours((long) voteSite.getTimeOffSet());
