2d8f9e1ca1f65001f95538676becad89d42ff2d8
BenCodez <benbergen12@gmail.com>
2021-06-29 21:50:10 -0400

    Update bstats class

--- VotingPlugin/src/com/bencodez/votingplugin/VotingPluginMetrics.java
+++ VotingPlugin/src/com/bencodez/votingplugin/VotingPluginMetrics.java
@@ -1,5 +1,7 @@
 package com.bencodez.votingplugin;
 
+import java.util.concurrent.Callable;
+
 import com.bencodez.advancedcore.api.metrics.BStatsMetrics;
 import com.bencodez.advancedcore.api.rewards.RewardHandler;
 import com.bencodez.votingplugin.user.UserManager;
@@ -7,12 +9,12 @@ import com.bencodez.votingplugin.user.UserManager;
 public class VotingPluginMetrics {
 
 	public void load(VotingPluginMain plugin) {
-		BStatsMetrics metrics = new BStatsMetrics(plugin);
+		BStatsMetrics metrics = new BStatsMetrics(plugin, 38);
 
-		metrics.addCustomChart(new BStatsMetrics.SimplePie("extrarewards_firstvote") {
+		metrics.addCustomChart(new BStatsMetrics.SimplePie("extrarewards_firstvote", new Callable<String>() {
 
 			@Override
-			public String getValue() {
+			public String call() throws Exception {
 				if (RewardHandler.getInstance().hasRewards(plugin.getSpecialRewardsConfig().getData(),
 						plugin.getSpecialRewardsConfig().getFirstVoteRewardsPath())) {
 					return "True";
@@ -20,11 +22,11 @@ public class VotingPluginMetrics {
 					return "False";
 				}
 			}
-		});
-		metrics.addCustomChart(new BStatsMetrics.SimplePie("extrarewards_everysite") {
+		}));
+		metrics.addCustomChart(new BStatsMetrics.SimplePie("extrarewards_everysite", new Callable<String>() {
 
 			@Override
-			public String getValue() {
+			public String call() throws Exception {
 				if (RewardHandler.getInstance().hasRewards(plugin.getConfigVoteSites().getData(),
 						plugin.getConfigVoteSites().getEverySiteRewardPath())) {
 					return "True";
@@ -32,11 +34,11 @@ public class VotingPluginMetrics {
 					return "False";
 				}
 			}
-		});
-		metrics.addCustomChart(new BStatsMetrics.SimplePie("extrarewards_allsites") {
+		}));
+		metrics.addCustomChart(new BStatsMetrics.SimplePie("extrarewards_allsites", new Callable<String>() {
 
 			@Override
-			public String getValue() {
+			public String call() throws Exception {
 				if (RewardHandler.getInstance().hasRewards(plugin.getSpecialRewardsConfig().getData(),
 						plugin.getSpecialRewardsConfig().getAllSitesRewardPath())) {
 					return "True";
@@ -44,11 +46,11 @@ public class VotingPluginMetrics {
 					return "False";
 				}
 			}
-		});
-		metrics.addCustomChart(new BStatsMetrics.SimplePie("extrarewards_cumulative") {
+		}));
+		metrics.addCustomChart(new BStatsMetrics.SimplePie("extrarewards_cumulative", new Callable<String>() {
 
 			@Override
-			public String getValue() {
+			public String call() throws Exception {
 				if (plugin.getSpecialRewardsConfig().getCumulativeVotes().size() == 0) {
 					return "False";
 				} else {
@@ -60,22 +62,22 @@ public class VotingPluginMetrics {
 					return "False";
 				}
 			}
-		});
-		metrics.addCustomChart(new BStatsMetrics.SimplePie("extrarewards_voteparty") {
+		}));
+		metrics.addCustomChart(new BStatsMetrics.SimplePie("extrarewards_voteparty", new Callable<String>() {
 
 			@Override
-			public String getValue() {
+			public String call() throws Exception {
 				if (!plugin.getSpecialRewardsConfig().getVotePartyEnabled()) {
 					return "False";
 				} else {
 					return "True";
 				}
 			}
-		});
-		metrics.addCustomChart(new BStatsMetrics.SimplePie("extrarewards_milestone") {
+		}));
+		metrics.addCustomChart(new BStatsMetrics.SimplePie("extrarewards_milestone", new Callable<String>() {
 
 			@Override
-			public String getValue() {
+			public String call() throws Exception {
 				if (plugin.getSpecialRewardsConfig().getMilestoneVotes().size() == 0) {
 					return "False";
 				} else {
@@ -87,11 +89,11 @@ public class VotingPluginMetrics {
 					return "False";
 				}
 			}
-		});
-		metrics.addCustomChart(new BStatsMetrics.SimplePie("extrarewards_anysitereward") {
+		}));
+		metrics.addCustomChart(new BStatsMetrics.SimplePie("extrarewards_anysitereward", new Callable<String>() {
 
 			@Override
-			public String getValue() {
+			public String call() throws Exception {
 				if (RewardHandler.getInstance().hasRewards(plugin.getSpecialRewardsConfig().getData(),
 						plugin.getSpecialRewardsConfig().getAnySiteRewardsPath())) {
 					return "True";
@@ -99,11 +101,11 @@ public class VotingPluginMetrics {
 					return "False";
 				}
 			}
-		});
-		metrics.addCustomChart(new BStatsMetrics.SimplePie("extrarewards_votestreakday") {
+		}));
+		metrics.addCustomChart(new BStatsMetrics.SimplePie("extrarewards_votestreakday", new Callable<String>() {
 
 			@Override
-			public String getValue() {
+			public String call() throws Exception {
 				for (String s : plugin.getSpecialRewardsConfig().getVoteStreakVotes("Day")) {
 
 					if (plugin.getSpecialRewardsConfig().getVoteStreakRewardEnabled("Day", s)
@@ -115,12 +117,12 @@ public class VotingPluginMetrics {
 				}
 				return "False";
 			}
-		});
+		}));
 
-		metrics.addCustomChart(new BStatsMetrics.SimplePie("extrarewards_votestreakweek") {
+		metrics.addCustomChart(new BStatsMetrics.SimplePie("extrarewards_votestreakweek", new Callable<String>() {
 
 			@Override
-			public String getValue() {
+			public String call() throws Exception {
 				for (String s : plugin.getSpecialRewardsConfig().getVoteStreakVotes("Week")) {
 
 					if (plugin.getSpecialRewardsConfig().getVoteStreakRewardEnabled("Week", s)
@@ -132,12 +134,12 @@ public class VotingPluginMetrics {
 
 				return "False";
 			}
-		});
+		}));
 
-		metrics.addCustomChart(new BStatsMetrics.SimplePie("extrarewards_votestreakmonth") {
+		metrics.addCustomChart(new BStatsMetrics.SimplePie("extrarewards_votestreakmonth", new Callable<String>() {
 
 			@Override
-			public String getValue() {
+			public String call() throws Exception {
 				for (String s : plugin.getSpecialRewardsConfig().getVoteStreakVotes("Month")) {
 
 					if (plugin.getSpecialRewardsConfig().getVoteStreakRewardEnabled("Month", s)
@@ -149,33 +151,33 @@ public class VotingPluginMetrics {
 
 				return "False";
 			}
-		});
-		metrics.addCustomChart(new BStatsMetrics.SimplePie("numberofsites") {
+		}));
+		metrics.addCustomChart(new BStatsMetrics.SimplePie("numberofsites", new Callable<String>() {
 
 			@Override
-			public String getValue() {
+			public String call() throws Exception {
 				return "" + plugin.getVoteSites().size();
 			}
-		});
-		metrics.addCustomChart(new BStatsMetrics.SimplePie("numberofrewards") {
+		}));
+		metrics.addCustomChart(new BStatsMetrics.SimplePie("numberofrewards", new Callable<String>() {
 
 			@Override
-			public String getValue() {
+			public String call() throws Exception {
 				return "" + RewardHandler.getInstance().getRewards().size();
 			}
-		});
-		metrics.addCustomChart(new BStatsMetrics.SimplePie("autocreatevotesites") {
+		}));
+		metrics.addCustomChart(new BStatsMetrics.SimplePie("autocreatevotesites", new Callable<String>() {
 
 			@Override
-			public String getValue() {
+			public String call() throws Exception {
 				return "" + plugin.getConfigFile().isAutoCreateVoteSites();
 			}
-		});
+		}));
 
-		metrics.addCustomChart(new BStatsMetrics.SimplePie("numberofusers") {
+		metrics.addCustomChart(new BStatsMetrics.SimplePie("numberofusers", new Callable<String>() {
 
 			@Override
-			public String getValue() {
+			public String call() throws Exception {
 				int total = UserManager.getInstance().getAllUUIDs().size();
 				if (total < 1000) {
 					return "<1000";
@@ -185,113 +187,113 @@ public class VotingPluginMetrics {
 					return "1000-10000";
 				}
 			}
-		});
-		metrics.addCustomChart(new BStatsMetrics.SimplePie("data_storage") {
+		}));
+		metrics.addCustomChart(new BStatsMetrics.SimplePie("data_storage", new Callable<String>() {
 
 			@Override
-			public String getValue() {
+			public String call() throws Exception {
 				return plugin.getOptions().getStorageType().toString();
 			}
-		});
-		metrics.addCustomChart(new BStatsMetrics.SimplePie("DisableCheckOnWorldChange") {
+		}));
+		metrics.addCustomChart(new BStatsMetrics.SimplePie("DisableCheckOnWorldChange", new Callable<String>() {
 
 			@Override
-			public String getValue() {
+			public String call() throws Exception {
 				return "" + plugin.getOptions().isDisableCheckOnWorldChange();
 			}
-		});
-		metrics.addCustomChart(new BStatsMetrics.SimplePie("votereminding_enabled") {
+		}));
+		metrics.addCustomChart(new BStatsMetrics.SimplePie("votereminding_enabled", new Callable<String>() {
 
 			@Override
-			public String getValue() {
+			public String call() throws Exception {
 				return "" + plugin.getConfigFile().getVoteRemindingEnabled();
 			}
-		});
-		metrics.addCustomChart(new BStatsMetrics.SimplePie("LoadTopVoter_Monthly") {
+		}));
+		metrics.addCustomChart(new BStatsMetrics.SimplePie("LoadTopVoter_Monthly", new Callable<String>() {
 
 			@Override
-			public String getValue() {
+			public String call() throws Exception {
 				return "" + plugin.getConfigFile().getLoadTopVoterMonthly();
 			}
-		});
-		metrics.addCustomChart(new BStatsMetrics.SimplePie("LoadTopVoter_Weekly") {
+		}));
+		metrics.addCustomChart(new BStatsMetrics.SimplePie("LoadTopVoter_Weekly", new Callable<String>() {
 
 			@Override
-			public String getValue() {
+			public String call() throws Exception {
 				return "" + plugin.getConfigFile().getLoadTopVoterWeekly();
 			}
-		});
-		metrics.addCustomChart(new BStatsMetrics.SimplePie("LoadTopVoter_Daily") {
+		}));
+		metrics.addCustomChart(new BStatsMetrics.SimplePie("LoadTopVoter_Daily", new Callable<String>() {
 
 			@Override
-			public String getValue() {
+			public String call() throws Exception {
 				return "" + plugin.getConfigFile().getLoadTopVoterDaily();
 			}
-		});
-		metrics.addCustomChart(new BStatsMetrics.SimplePie("bungeemethod") {
+		}));
+		metrics.addCustomChart(new BStatsMetrics.SimplePie("bungeemethod", new Callable<String>() {
 
 			@Override
-			public String getValue() {
+			public String call() throws Exception {
 				if (plugin.getBungeeSettings().isUseBungeecoord()) {
 					return "" + plugin.getBungeeHandler().getMethod().toString();
 				} else {
 					return "Disabled";
 				}
 			}
-		});
+		}));
 		if (plugin.getBungeeSettings().isUseBungeecoord()) {
-			metrics.addCustomChart(new BStatsMetrics.SimplePie("bungeebroadcast") {
+			metrics.addCustomChart(new BStatsMetrics.SimplePie("bungeebroadcast", new Callable<String>() {
 
 				@Override
-				public String getValue() {
+				public String call() throws Exception {
 					return "" + plugin.getBungeeSettings().isBungeeBroadcast();
 				}
-			});
-			metrics.addCustomChart(new BStatsMetrics.SimplePie("bungeebroadcastalways") {
+			}));
+			metrics.addCustomChart(new BStatsMetrics.SimplePie("bungeebroadcastalways", new Callable<String>() {
 
 				@Override
-				public String getValue() {
+				public String call() throws Exception {
 					return "" + plugin.getBungeeSettings().isBungeeBroadcastAlways();
 				}
-			});
+			}));
 
-			metrics.addCustomChart(new BStatsMetrics.SimplePie("perserverrewards") {
+			metrics.addCustomChart(new BStatsMetrics.SimplePie("perserverrewards", new Callable<String>() {
 
 				@Override
-				public String getValue() {
+				public String call() throws Exception {
 					return "" + plugin.getBungeeSettings().isPerServerRewards();
 				}
-			});
-			metrics.addCustomChart(new BStatsMetrics.SimplePie("perserverpoints") {
+			}));
+			metrics.addCustomChart(new BStatsMetrics.SimplePie("perserverpoints", new Callable<String>() {
 
 				@Override
-				public String getValue() {
+				public String call() throws Exception {
 					return "" + plugin.getBungeeSettings().isPerServerPoints();
 				}
-			});
-			metrics.addCustomChart(new BStatsMetrics.SimplePie("triggervotifierevent") {
+			}));
+			metrics.addCustomChart(new BStatsMetrics.SimplePie("triggervotifierevent", new Callable<String>() {
 
 				@Override
-				public String getValue() {
+				public String call() throws Exception {
 					return "" + plugin.getBungeeSettings().isTriggerVotifierEvent();
 				}
-			});
+			}));
 		}
 
-		metrics.addCustomChart(new BStatsMetrics.SimplePie("geyserprefixsupport") {
+		metrics.addCustomChart(new BStatsMetrics.SimplePie("geyserprefixsupport", new Callable<String>() {
 
 			@Override
-			public String getValue() {
+			public String call() throws Exception {
 				return "" + plugin.getOptions().isGeyserPrefixSupport();
 			}
-		});
+		}));
 
-		metrics.addCustomChart(new BStatsMetrics.SimplePie("using_dev_build") {
+		metrics.addCustomChart(new BStatsMetrics.SimplePie("using_dev_build", new Callable<String>() {
 
 			@Override
-			public String getValue() {
+			public String call() throws Exception {
 				return "" + plugin.getProfile().contains("dev");
 			}
-		});
+		}));
 	}
 }
