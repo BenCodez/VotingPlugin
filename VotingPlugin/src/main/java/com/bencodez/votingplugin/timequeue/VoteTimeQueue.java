699444d63031bc88c6062da8d17aef57a5bec80b
Ben <benbergen12@gmail.com>
2026-07-19 13:10:12 -0600

    Add vote IDs to time queue entries

--- VotingPlugin/src/main/java/com/bencodez/votingplugin/timequeue/VoteTimeQueue.java
+++ VotingPlugin/src/main/java/com/bencodez/votingplugin/timequeue/VoteTimeQueue.java
@@ -1,5 +1,7 @@
 package com.bencodez.votingplugin.timequeue;
 
+import java.util.UUID;
+
 import lombok.Getter;
 import lombok.Setter;
 
@@ -13,8 +15,16 @@ public class VoteTimeQueue {
 	@Getter
 	@Setter
 	private long time;
+	@Getter
+	@Setter
+	private UUID voteId;
 
 	public VoteTimeQueue(String name, String service, long time) {
+		this(null, name, service, time);
+	}
+
+	public VoteTimeQueue(UUID voteId, String name, String service, long time) {
+		this.voteId = voteId;
 		this.name = name;
 		this.service = service;
 		this.time = time;
