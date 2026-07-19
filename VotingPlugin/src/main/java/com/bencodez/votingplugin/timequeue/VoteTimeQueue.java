7d8d14e801b3ae32e509d235debc48b51aee3e3e
Ben <benbergen12@gmail.com>
2026-07-19 13:18:12 -0600

    Document time queue vote identity

--- VotingPlugin/src/main/java/com/bencodez/votingplugin/timequeue/VoteTimeQueue.java
+++ VotingPlugin/src/main/java/com/bencodez/votingplugin/timequeue/VoteTimeQueue.java
@@ -5,6 +5,9 @@ import java.util.UUID;
 import lombok.Getter;
 import lombok.Setter;
 
+/**
+ * Represents a vote delayed while a proxy time change is active.
+ */
 public class VoteTimeQueue {
 	@Getter
 	@Setter
@@ -19,10 +22,25 @@ public class VoteTimeQueue {
 	@Setter
 	private UUID voteId;
 
+	/**
+	 * Creates a legacy-compatible queued vote without an identifier.
+	 *
+	 * @param name player name
+	 * @param service service site
+	 * @param time vote timestamp
+	 */
 	public VoteTimeQueue(String name, String service, long time) {
 		this(null, name, service, time);
 	}
 
+	/**
+	 * Creates a queued vote with its original identifier.
+	 *
+	 * @param voteId unique vote identifier
+	 * @param name player name
+	 * @param service service site
+	 * @param time vote timestamp
+	 */
 	public VoteTimeQueue(UUID voteId, String name, String service, long time) {
 		this.voteId = voteId;
 		this.name = name;
