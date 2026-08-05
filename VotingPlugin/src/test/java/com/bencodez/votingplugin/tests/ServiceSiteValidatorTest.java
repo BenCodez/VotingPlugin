dcbf1e9e90557e15ddfec4ba1a67935ed5dd7ca4
Ben <benbergen12@gmail.com>
2026-08-05 16:59:29 -0600

    Test invisible combining-only service names

--- VotingPlugin/src/test/java/com/bencodez/votingplugin/tests/ServiceSiteValidatorTest.java
+++ VotingPlugin/src/test/java/com/bencodez/votingplugin/tests/ServiceSiteValidatorTest.java
@@ -14,7 +14,7 @@ class ServiceSiteValidatorTest {
 		for (String serviceSite : new String[] { "PlanetMinecraft.com", "Minecraft Server List", "Crafty.gg",
 				"https://example.com/vote", "https://list.example/vote?id=1&source=proxy#top",
 				"https://example.com/search?q=site%20name+network", "site_name-2", "Site, Other; Network!",
-				"Serviço de votação", "Site\u00A0Name" }) {
+				"Serviço de votação", "Site\u00A0Name", "Site\uFE0F", "Cafe\u0301" }) {
 			assertTrue(ServiceSiteValidator.isValid(serviceSite), serviceSite);
 		}
 	}
@@ -36,6 +36,10 @@ class ServiceSiteValidatorTest {
 		assertFalse(ServiceSiteValidator.isValid("\u2007"));
 		assertFalse(ServiceSiteValidator.isValid("\u202F"));
 		assertFalse(ServiceSiteValidator.isValid(" \u00A0\u2007\u202F "));
+		assertFalse(ServiceSiteValidator.isValid("\uFE0F"));
+		assertFalse(ServiceSiteValidator.isValid("\u034F"));
+		assertFalse(ServiceSiteValidator.isValid("\u0301"));
+		assertFalse(ServiceSiteValidator.isValid("\uFE0F\u034F\u0301"));
 		assertFalse(ServiceSiteValidator.isValid("A".repeat(2049)));
 		assertTrue(ServiceSiteValidator.isValid("A".repeat(2048)));
 	}
