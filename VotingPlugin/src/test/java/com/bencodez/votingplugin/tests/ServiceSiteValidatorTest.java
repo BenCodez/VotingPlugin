dca3e775736452813e24c53a362dd2f9e6161191
Ben <benbergen12@gmail.com>
2026-08-05 16:34:26 -0600

    Test URL-compatible service site names

--- VotingPlugin/src/test/java/com/bencodez/votingplugin/tests/ServiceSiteValidatorTest.java
+++ VotingPlugin/src/test/java/com/bencodez/votingplugin/tests/ServiceSiteValidatorTest.java
@@ -12,16 +12,17 @@ class ServiceSiteValidatorTest {
 	@Test
 	void acceptsCommonServiceSiteNames() {
 		for (String serviceSite : new String[] { "PlanetMinecraft.com", "Minecraft Server List", "Crafty.gg",
-				"https://example.com/vote", "site_name-2" }) {
+				"https://example.com/vote", "https://list.example/vote?id=1&source=proxy#top",
+				"https://example.com/search?q=site%20name+network", "site_name-2", "Site, Other; Network!",
+				"Serviço de votação" }) {
 			assertTrue(ServiceSiteValidator.isValid(serviceSite), serviceSite);
 		}
 	}
 
 	@Test
 	void rejectsUnsupportedCharacters() {
-		for (String serviceSite : new String[] { "[Javascript=1]", "Site's", "\"Site\"", "Site%player%",
-				"Site,Other", "Site;Other", "Site#Comment", "Site`Name", "Site\\Name", "Site\nName",
-				"Site\tName" }) {
+		for (String serviceSite : new String[] { "[Javascript=1]", "Site's", "\"Site\"", "Site`Name",
+				"Site\\Name", "Site\nName", "Site\tName", "Site\u0000Name", "Site\u200BName" }) {
 			assertFalse(ServiceSiteValidator.isValid(serviceSite), serviceSite);
 		}
 	}
@@ -31,7 +32,7 @@ class ServiceSiteValidatorTest {
 		assertFalse(ServiceSiteValidator.isValid(null));
 		assertFalse(ServiceSiteValidator.isValid(""));
 		assertFalse(ServiceSiteValidator.isValid(" "));
-		assertFalse(ServiceSiteValidator.isValid("A".repeat(129)));
-		assertTrue(ServiceSiteValidator.isValid("A".repeat(128)));
+		assertFalse(ServiceSiteValidator.isValid("A".repeat(2049)));
+		assertTrue(ServiceSiteValidator.isValid("A".repeat(2048)));
 	}
 }
