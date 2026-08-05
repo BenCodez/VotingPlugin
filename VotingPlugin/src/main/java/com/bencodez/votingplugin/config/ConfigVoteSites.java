f18b442aec9d5ff6b7e334e190cad595721b23e5
Ben <benbergen12@gmail.com>
2026-08-05 16:43:34 -0600

    Return vote site generation status

--- VotingPlugin/src/main/java/com/bencodez/votingplugin/config/ConfigVoteSites.java
+++ VotingPlugin/src/main/java/com/bencodez/votingplugin/config/ConfigVoteSites.java
@@ -48,11 +48,21 @@ public class ConfigVoteSites extends YMLFile {
 	 * @param siteName the site name
 	 */
 	public void generateVoteSite(String siteName) {
+		tryGenerateVoteSite(siteName);
+	}
+
+	/**
+	 * Attempts to generate a vote site.
+	 *
+	 * @param siteName the site name
+	 * @return {@code true} if the site was generated
+	 */
+	public boolean tryGenerateVoteSite(String siteName) {
 		if (plugin.getConfigFile().isAutoCreateVoteSites()) {
 			if (!ServiceSiteValidator.isValid(siteName)) {
 				plugin.getLogger().warning("Unable to generate vote site with unsupported name '"
 						+ ServiceSiteValidator.sanitizeForLog(siteName) + "'");
-				return;
+				return false;
 			}
 			String org = siteName;
 			siteName = siteName.replaceAll("[\\.\\s]+", "_");
@@ -147,7 +157,9 @@ public class ConfigVoteSites extends YMLFile {
 							+ ", please check console for details"));
 				}
 			}
+			return true;
 		}
+		return false;
 	}
 
 	/**
