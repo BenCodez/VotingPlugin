d5a7f9f906742fa48336e3ea68f73d7b2d3b8087
Ben <benbergen12@gmail.com>
2026-08-23 14:10:28 -0600

    Remove generated comments from CommandVote

--- VotingPlugin/src/main/java/com/bencodez/votingplugin/commands/executers/CommandVote.java
+++ VotingPlugin/src/main/java/com/bencodez/votingplugin/commands/executers/CommandVote.java
@@ -8,49 +8,23 @@ import com.bencodez.advancedcore.api.command.CommandHandler;
 import com.bencodez.simpleapi.messages.MessageAPI;
 import com.bencodez.votingplugin.VotingPluginMain;
 
-// TODO: Auto-generated Javadoc
-/**
- * The Class CommandVote.
- */
 public class CommandVote implements CommandExecutor {
 
-	/** The instance. */
 	private static CommandVote instance = new CommandVote();
 
-	/** The plugin. */
 	private static VotingPluginMain plugin;
 
-	/**
-	 * Gets the single instance of CommandVote.
-	 *
-	 * @return single instance of CommandVote
-	 */
 	public static CommandVote getInstance() {
 		return instance;
 	}
 
-	/**
-	 * Instantiates a new command vote.
-	 */
 	private CommandVote() {
 	}
 
-	/**
-	 * Instantiates a new command vote.
-	 *
-	 * @param plugin the plugin
-	 */
 	public CommandVote(VotingPluginMain plugin) {
 		CommandVote.plugin = plugin;
 	}
 
-	/*
-	 * (non-Javadoc)
-	 *
-	 * @see org.bukkit.command.CommandExecutor#onCommand(org.bukkit.command.
-	 * CommandSender , org.bukkit.command.Command, java.lang.String,
-	 * java.lang.String[])
-	 */
 	@Override
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
 
