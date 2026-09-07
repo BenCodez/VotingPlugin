fd1b2b125f95017127c19a68bfad539a2d874cfe
Ben <benbergen12@gmail.com>
2026-09-07 15:38:21 -0600

    Bound remote service data and vote work

--- VotingPlugin/src/main/java/com/bencodez/votingplugin/VotingPluginMain.java
+++ VotingPlugin/src/main/java/com/bencodez/votingplugin/VotingPluginMain.java
@@ -98,6 +98,7 @@ import com.bencodez.votingplugin.placeholders.VotingPluginExpansion;
 import com.bencodez.votingplugin.presets.VoteSitePresetSetupHandler;
 import com.bencodez.votingplugin.proxy.control.HostedControlManager;
 import com.bencodez.votingplugin.util.ControlCredentialFile.PendingAutoEnrollment;
+import com.bencodez.votingplugin.util.BoundedScheduledExecutor;
 import com.bencodez.votingplugin.rewards.VotingPluginRewardRegistrar;
 import com.bencodez.votingplugin.servicesites.ServiceSiteHandler;
 import com.bencodez.votingplugin.signs.Signs;
@@ -561,7 +562,7 @@ public class VotingPluginMain extends AdvancedCorePlugin {
 	}
 
 	private void loadVoteTimer() {
-		voteTimer = Executors.newSingleThreadScheduledExecutor();
+		voteTimer = new BoundedScheduledExecutor(1, 256);
 	}
 
 	@Deprecated
