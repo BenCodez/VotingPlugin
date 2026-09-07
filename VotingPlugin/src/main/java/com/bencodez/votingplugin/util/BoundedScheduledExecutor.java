f394d1b2fa33e45aae687ef8765709e344386289
Ben <benbergen12@gmail.com>
2026-09-07 16:36:24 -0600

    Address resource-bound review feedback

--- VotingPlugin/src/main/java/com/bencodez/votingplugin/util/BoundedScheduledExecutor.java
+++ VotingPlugin/src/main/java/com/bencodez/votingplugin/util/BoundedScheduledExecutor.java
@@ -5,6 +5,8 @@ import java.util.concurrent.ScheduledFuture;
 import java.util.concurrent.ScheduledThreadPoolExecutor;
 import java.util.concurrent.Semaphore;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.Delayed;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 /** A scheduled executor with an admission limit for queued and running one-shot tasks. */
 public final class BoundedScheduledExecutor extends ScheduledThreadPoolExecutor {
@@ -24,17 +26,45 @@ public final class BoundedScheduledExecutor extends ScheduledThreadPoolExecutor 
 		if (!permits.tryAcquire()) {
 			throw new RejectedExecutionException("Vote task capacity exhausted");
 		}
+		AtomicBoolean released = new AtomicBoolean();
 		try {
-			return super.schedule(() -> {
+			ScheduledFuture<?> delegate = super.schedule(() -> {
 				try {
 					command.run();
 				} finally {
-					permits.release();
+					releaseOnce(released);
 				}
 			}, delay, unit);
+			return new PermitReleasingFuture(delegate, released);
 		} catch (RuntimeException ex) {
 			permits.release();
 			throw ex;
 		}
+	}
+
+	private void releaseOnce(AtomicBoolean released) {
+		if (released.compareAndSet(false, true)) {
+			permits.release();
+		}
+	}
+
+	private final class PermitReleasingFuture implements ScheduledFuture<Object> {
+		private final ScheduledFuture<?> delegate;
+		private final AtomicBoolean released;
+		private PermitReleasingFuture(ScheduledFuture<?> delegate, AtomicBoolean released) {
+			this.delegate = delegate;
+			this.released = released;
+		}
+		@Override public boolean cancel(boolean mayInterruptIfRunning) {
+			boolean cancelled = delegate.cancel(mayInterruptIfRunning);
+			if (cancelled) releaseOnce(released);
+			return cancelled;
+		}
+		@Override public boolean isCancelled() { return delegate.isCancelled(); }
+		@Override public boolean isDone() { return delegate.isDone(); }
+		@Override public Object get() throws java.lang.InterruptedException, java.util.concurrent.ExecutionException { return delegate.get(); }
+		@Override public Object get(long timeout, TimeUnit unit) throws java.lang.InterruptedException, java.util.concurrent.ExecutionException, java.util.concurrent.TimeoutException { return delegate.get(timeout, unit); }
+		@Override public long getDelay(TimeUnit unit) { return delegate.getDelay(unit); }
+		@Override public int compareTo(Delayed other) { return delegate.compareTo(other); }
 	}
 }
